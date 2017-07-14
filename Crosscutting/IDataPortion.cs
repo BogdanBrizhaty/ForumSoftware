fb750052b10a9977bec42abd21ae6fc559410098
BogdanBrizhaty <demaskinas@yandex.ua>
2017-07-14 21:58:17 +0300

    Crosscutting library. Contains types used accross all over the application. DataPortion data-transfering type and Ordering helper, which specifies custom sorter for data

---
+++ Crosscutting/IDataPortion.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace ForumSoftware.Crosscutting
+{
+    /// <summary>
+    /// Interface for DataPortion passing
+    /// </summary>
+    /// <typeparam name="TItem"></typeparam>
+    public interface IDataPortion<TItem> where TItem : class
+    {
+        IEnumerable<TItem> Items { get; }
+        int TotalItems { get; }
+    }
+}
