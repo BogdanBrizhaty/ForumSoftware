6ae225746cd2528134382c151bf291f30c539f0a
BogdanBrizhaty <demaskinas@yandex.ua>
2017-07-15 22:55:22 +0300

    DI automapper resolver

---
+++ ForumSoftware.DependenctyResolver/InjectionModules/AutoMapperNInjectModule.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ForumSoftware.ClassMapping;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependenctyResolver.InjectionModules
+{
+    public class AutoMapperNinjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            IMapper _mapper = AutoMapperConfigurator.InitializeAutoMapper().CreateMapper();
+            Bind<IMapper>().ToConstant(_mapper).InSingletonScope();
+        }
+    }
+}
