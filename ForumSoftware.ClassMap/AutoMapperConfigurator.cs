a25b9e789a8f2c57ff6ff839511d59ecbc4ec008
BogdanBrizhaty <demaskinas@yandex.ua>
2017-07-15 22:18:55 +0300

    AutoMapper added

---
+++ ForumSoftware.ClassMap/AutoMapperConfigurator.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using ForumSoftware.ClassMapping.MappingProfiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForumSoftware.ClassMapping
+{
+    public static class AutoMapperConfigurator
+    {
+        public static MapperConfiguration InitializeAutoMapper()
+        {
+            var config = new MapperConfiguration(
+                //cfg => cfg.Add
+                cfg =>
+                {
+                    cfg.AddProfile(new EntityToDTOMappingProfile());
+                    cfg.AddProfile(new DTOtoEntityMappingProfile());
+                    // initialize other mapping profiles
+                }
+                );
+            return config;
+        }
+    }
+}
---
+++ ForumSoftware.ClassMap/MappingProfiles/DTOtoViewModelMappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using ForumSoftware.BLL.DTO;
+using ForumSoftware.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForumSoftware.ClassMapping.MappingProfiles
+{
+    /*
+    public class DALtoBLLInteractionProfile : Profile
+    {
+        public DALtoBLLInteractionProfile()
+        {
+            CreateMap<ApplicationUser, UserCredentialsDTO>();
+            CreateMap<UserProfile, UserProfileDTO>();
+
+            // other maps goe here
+        }
+    } */
+}
