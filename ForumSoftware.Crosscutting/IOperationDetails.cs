ce17ed4f51b50fbe82245f7a5bc8e6de027506f4
BogdanBrizhaty <demaskinas@yandex.ua>
2017-07-15 22:20:00 +0300

    Crosscutting upd.

---
+++ ForumSoftware.Crosscutting/IOperationDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForumSoftware.Crosscutting
+{
+    public interface IOperationDetails
+    {
+        OperationFailureError Error { get; }
+        bool Succeeded { get; }
+    }
+}
---
+++ ForumSoftware.Crosscutting/OperationDetails.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForumSoftware.Crosscutting
+{
+    public class OperationDetails : IOperationDetails
+    {
+        private OperationFailureError _error;
+        public OperationDetails(OperationFailureError error)
+        {
+            _error = error;
+        }
+        public OperationDetails()
+        {
+            _error = null;
+        }
+        public OperationFailureError Error
+        {
+            get { return _error; }
+        }
+
+        public bool Succeeded
+        {
+            get { return (_error == null); }
+        }
+    }
+}
